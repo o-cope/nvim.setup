eb89c55622d2c2feffbea5ffa183385cb39fa1cb
oliver <ocollinscope@gmail.com>
2025-01-30 17:32:52 +0000

    <baseline>: added toggling and saving theme state

--- lua/plugins/colourtheme.lua
+++ lua/plugins/colourtheme.lua
@@ -3,7 +3,24 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    -- Set the initial color mode to dark and contrast to 'hard'
+    local theme_file = vim.fn.stdpath 'config' .. '/gruvbox_theme_state.txt'
+
+    -- Initialize the theme state by reading the file (if it exists)
+    local function read_theme_state()
+      local f = io.open(theme_file, 'r')
+      if f then
+        local state = f:read '*a'
+        f:close()
+        return state:match '^%s*(.-)%s*$' -- Trim any extra whitespace
+      else
+        return 'dark'                     -- Default to 'dark' if no file exists
+      end
+    end
+
+    -- Set the initial theme based on the file state
+    local current_theme = read_theme_state()
+
+    -- Set contrast options
     vim.g.gruvbox_contrast_dark = 'hard'
     vim.g.gruvbox_contrast_light = 'hard'
     vim.g.gruvbox_invert_selection = false
@@ -13,29 +30,40 @@ return {
     vim.g.gruvbox_sign_column = 'bg0'
 
     -- Set the initial background and colorscheme
-    vim.cmd [[
-      set background=dark
-      colorscheme gruvbox
-    ]]
+    if current_theme == 'light' then
+      vim.cmd [[
+        set background=light
+        colorscheme gruvbox
+      ]]
+    else
+      vim.cmd [[
+        set background=dark
+        colorscheme gruvbox
+      ]]
+    end
 
     -- Toggle function between light and dark modes
     function _G.toggle_gruvbox()
-      if vim.g.gruvbox_contrast_dark == 'hard' then
+      if vim.o.background == 'dark' then
         -- Switch to light mode with 'hard' contrast
+        vim.o.background = 'light'
         vim.g.gruvbox_contrast_dark = 'soft'
         vim.g.gruvbox_contrast_light = 'hard'
-        vim.cmd [[
-          set background=light
-          colorscheme gruvbox
-        ]]
+        vim.cmd [[colorscheme gruvbox]]
+        -- Save the theme state to the file
+        local f = io.open(theme_file, 'w')
+        f:write 'light'
+        f:close()
       else
         -- Switch to dark mode with 'hard' contrast
+        vim.o.background = 'dark'
         vim.g.gruvbox_contrast_dark = 'hard'
         vim.g.gruvbox_contrast_light = 'soft'
-        vim.cmd [[
-          set background=dark
-          colorscheme gruvbox
-        ]]
+        vim.cmd [[colorscheme gruvbox]]
+        -- Save the theme state to the file
+        local f = io.open(theme_file, 'w')
+        f:write 'dark'
+        f:close()
       end
     end
 
@@ -43,3 +71,4 @@ return {
     vim.api.nvim_set_keymap('n', '<leader>tb', ':lua toggle_gruvbox()<CR>', { noremap = true, silent = true })
   end,
 }
+
