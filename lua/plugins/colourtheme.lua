ac71696c0716eba8298e035bae648af64afd55e7
oliver <ocollinscope@gmail.com>
2025-01-30 17:04:37 +0000

    <baseline>: added toggle for background type

--- lua/plugins/colourtheme.lua
+++ lua/plugins/colourtheme.lua
@@ -1,29 +1,45 @@
 return {
-  'morhetz/gruvbox', -- Correct Gruvbox plugin
+  'morhetz/gruvbox', -- Gruvbox plugin
   lazy = false,
   priority = 1000,
   config = function()
-    -- Example config in lua
-    vim.g.gruvbox_contrast_dark = 'hard' -- 'hard', 'medium', or 'soft'
-    vim.g.gruvbox_borders = true -- Set borders for windows
-    vim.g.gruvbox_disable_background = false -- Set to false to keep background
-    vim.g.gruvbox_italic = false -- Set to true if you want italic text
-    vim.g.gruvbox_bold = true -- Set to true if you want bold text
-    vim.g.gruvbox_sign_column = 'bg0' -- Sign column background color
-    vim.g.gruvbox_invert_selection = false -- Invert selection (set to true if you prefer)
+    -- Set the initial color mode to dark and contrast to 'hard'
+    vim.g.gruvbox_contrast_dark = 'hard'
+    vim.g.gruvbox_contrast_light = 'hard'
+    vim.g.gruvbox_invert_selection = false
+    vim.g.gruvbox_borders = true
+    vim.g.gruvbox_italic = false
+    vim.g.gruvbox_bold = true
+    vim.g.gruvbox_sign_column = 'bg0'
 
-    -- Load the colorscheme
-    vim.cmd [[colorscheme gruvbox]]
+    -- Set the initial background and colorscheme
+    vim.cmd [[
+      set background=dark
+      colorscheme gruvbox
+    ]]
 
-    -- Toggle background transparency
-    local bg_transparent = true
-
-    local toggle_transparency = function()
-      bg_transparent = not bg_transparent
-      vim.g.gruvbox_disable_background = bg_transparent
-      vim.cmd [[colorscheme gruvbox]] -- Switch the colorscheme
+    -- Toggle function between light and dark modes
+    function _G.toggle_gruvbox()
+      if vim.g.gruvbox_contrast_dark == 'hard' then
+        -- Switch to light mode with 'hard' contrast
+        vim.g.gruvbox_contrast_dark = 'soft'
+        vim.g.gruvbox_contrast_light = 'hard'
+        vim.cmd [[
+          set background=light
+          colorscheme gruvbox
+        ]]
+      else
+        -- Switch to dark mode with 'hard' contrast
+        vim.g.gruvbox_contrast_dark = 'hard'
+        vim.g.gruvbox_contrast_light = 'soft'
+        vim.cmd [[
+          set background=dark
+          colorscheme gruvbox
+        ]]
+      end
     end
 
-    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+    -- Map the keybinding to toggle Gruvbox theme (<leader>tb)
+    vim.api.nvim_set_keymap('n', '<leader>tb', ':lua toggle_gruvbox()<CR>', { noremap = true, silent = true })
   end,
 }
